d2df029b6e1bc49803d7c8df113d83ae531f3cb0
BernhardBaumrock <office@baumrock.com>
2024-01-13 18:19:32 +0100

    feat: make inputfield show prices as .toFixed(2)

--- InputfieldRockMoney.module.php
+++ InputfieldRockMoney.module.php
@@ -34,7 +34,9 @@ class InputfieldRockMoney extends InputfieldText
   {
     $html = parent::___render();
     $html = substr($html, 0, -2);
-    $html .= " onfucus='this.select()' onclick='this.select()' />";
+    $html .= " onfucus='this.select()' onclick='this.select()'";
+    $html .= " onblur='this.value = parseFloat(this.value).toFixed(2)' />";
+    $html .= "<script>$('#{$this->id}').val(parseFloat($('#{$this->id}').val()).toFixed(2))</script>";
     return $html;
   }
 
